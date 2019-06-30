adb8584a62834370fc12d0065b34fbd1521db629
Romain Dorgueil <romain@dorgueil.net>
2019-06-30 09:49:19 +0200

    Tests - add test for pytest version management.

--- tests/feature/test_feature_pytest.py
+++ tests/feature/test_feature_pytest.py
@@ -9,7 +9,7 @@ PACKAGE_NAME = "foo.bar"
 
 class TestPytestFeature(FeatureTestCase):
     feature_type = PytestFeature
-    required_features = {"make", "pytest"}
+    required_features = {"python", "make", "pytest"}
 
     def test_configure(self):
         feature, dispatcher = self.create_feature()
@@ -19,7 +19,7 @@ class TestPytestFeature(FeatureTestCase):
         assert feature.on_make_generate in listeners["medikit.feature.make.on_generate"]
 
     def test_on_make_generate(self):
-        pytest_feature, dispatcher = self.create_feature()
+        pytest_feature, _ = self.create_feature()
 
         event = MakefileEvent(PACKAGE_NAME, Makefile(), self.create_config())
         pytest_feature.on_make_generate(event)
@@ -28,6 +28,31 @@ class TestPytestFeature(FeatureTestCase):
         assert {"PYTEST", "PYTEST_OPTIONS"} == set(event.makefile.environ)
 
     def test_on_start(self):
-        feature, dispatcher = self.create_feature()
+        feature, _ = self.create_feature()
         event = ProjectEvent(config=self.create_config(), setup={"name": PACKAGE_NAME})
         feature.on_start(event)
+
+    def test_set_version(self):
+        # test without setting the version, should be the current default
+        config = self.create_config()
+        event = ProjectEvent(config=config, setup={"name": PACKAGE_NAME})
+        feature, _ = self.create_feature()
+        feature.on_python_generate(event)
+        assert list(config["python"].get_requirements(extra="dev")) == [
+            "coverage ~= 4.5",
+            "pytest ~= 4.6",
+            "pytest-cov ~= 2.7",
+        ]
+
+        # test with setting the version, should override the default
+        config = self.create_config()
+        config.require("pytest").set_version("~=5.0")
+        event = ProjectEvent(config=config, setup={"name": PACKAGE_NAME})
+        feature, _ = self.create_feature()
+        feature.on_python_generate(event)
+        assert list(config["python"].get_requirements(extra="dev")) == [
+            "coverage ~= 4.5",
+            "pytest ~= 5.0",
+            "pytest-cov ~= 2.7",
+        ]
+
