c9315693ca86a1c14bcdcaa2bf6a02d7ac2f4f32
Romain Dorgueil <romain@dorgueil.net>
2020-11-25 08:06:18 +0100

    Refactor medikit stubs so that IDEs understand that something will actually happen after calling those things.

--- medikit/__init__.py
+++ medikit/__init__.py
@@ -1,4 +1,6 @@
 # noinspection PyUnresolvedReferences
+from contextlib import contextmanager
+
 import piptools
 
 from medikit._version import __version__
@@ -7,16 +9,42 @@ on_start = "medikit.on_start"
 on_end = "medikit.on_end"
 
 
-def listen(*args, **kwargs):
-    raise NotImplementedError("This is a stub.")
+def listen(event_id, priority=0):
+    """
+    This is a stub that will be replaced during medikit execution by a real implementation.
+    A fake implementation is provided here so that IDEs understand better the Projectfile content.
+
+    See `whistle.dispatcher.EventDispatcher`.
+
+    """
+
+    def wrapper(listener):
+        return listener
+
+    return wrapper
 
 
 def require(*args):
-    raise NotImplementedError("This is a stub.")
+    """
+    This is a stub that will be replaced during medikit execution by a real implementation.
+    A fake implementation is provided here so that IDEs understand better the Projectfile content.
+
+    See `medikit.config.registry.ConfigurationRegistry.require`.
+
+    """
+    return args[0] if len(args) == 1 else args
 
 
-def pipeline(*args):
-    raise NotImplementedError("This is a stub.")
+@contextmanager
+def pipeline(name):
+    """
+    This is a stub that will be replaced during medikit execution by a real implementation.
+    A fake implementation is provided here so that IDEs understand better the Projectfile content.
+
+    See `medikit.config.registry.ConfigurationRegistry.pipeline`.
+
+    """
+    yield
 
 
 __all__ = ["__version__"]
